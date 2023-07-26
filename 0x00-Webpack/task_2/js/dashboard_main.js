175e131ffa83c0514da86cfec934f9d49de4f2ea
David Efod <davidefod@gmail.com>
2023-07-26 09:18:21 +0100

    fix dependency

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,26 +1,18 @@
-import $ from "jquery";
-import _ from "lodash";
 import '../css/main.css';
-import './image-component';
+import $ from 'jquery';
+import _ from 'lodash';
 
-const text1 = $("<p></p>").text("Holberton Dashboard");
-const text2 = $("<p></p>").text("Dashboard data for the students");
-const button = $("<button></button>").text("Click here to get started");
-const text3 = $("<p id='count'></p>");
-const text4 = $("<p></p>").text("Copyright - Holberton School");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
+let conunt = 0;
 
-const div = $("<div id='container'></div>");
-div.append(button, text3);
-$("body").append(text1, text2, div, text4);
-
-let sum = 0;
-
-function count(){
-    sum++;
-    $("#count").text(`${sum} clicks on the button`);
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
 };
 
-$(document).ready(function(){
-    $("button").on('click', _.debounce(count,5000));
-});
+$('button').on('click', _.debounce(updateCounter, 500));
--- 0x00-Webpack/task_2/js/image-component.js
+++
@@ -1,8 +0,0 @@
-import image from "../assets/holberton-logo.jpg";
-import $ from 'jquery';
-const logo = $("<div id='logo'></div>");
-const img = document.createElement("img");
-img.src = image;
-logo.append(img);
-
-$("body").prepend(logo);
