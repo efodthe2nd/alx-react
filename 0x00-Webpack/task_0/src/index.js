b7068570bbe640785c20758bb249109a25b3a287
David Efod <davidefod@gmail.com>
2023-07-26 09:33:14 +0100

    fix error

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,5 @@
-import $ from "jquery";
+import $ from 'jquery';
 
-const text1 = $("<p></p>").text("Holberton Dashboard");
-const text2 = $("<p></p>").text("Dashboard data for the students");
-const text3 = $("<p></p>").text("Copyright - Holberton School");
-
-$("body").append(text1, text2, text3);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
