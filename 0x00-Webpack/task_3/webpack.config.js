4b10e39fc8f7c5d499ffb54b26f03cc19e2b1c21
David Efod <davidefod@gmail.com>
2023-07-26 15:40:10 +0100

    task_3

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,66 +1,68 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: "inline-source-map",
-    entry: {
-        header: {
-            import: './js/header.js',
-            dependOn: 'shared',
-        },
-        body: {
-            import: './js/body.js',
-            dependOn: 'shared',
-        },
-        footer: {
-            import: './js/footer.js',
-            dependOn: 'shared'
-        },
-        shared: 'jquery',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'public')
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    performance: {
-        hints: "warning",
-        maxAssetSize: 512000,
-        maxEntrypointSize: 512000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-            },
-        ]
-    },
-    devServer: {
-        port: 8564,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Webpack Output",
-        }),
-        new CleanWebpackPlugin(),
-    ],
+	plugins: [
+		new HTMLWebpackPlugin({
+			filename: './index.html',
+		}),
+		new CleanWebpackPlugin(),
+		],
+	devtool: 'inline-source-map',
+	mode: 'development',
+	entry: {
+		header: {
+			import: './modules/header/header.js',
+			dependOn: 'shared',
+		},
+		body: {
+			import: './modules/body/body.js',
+			dependOn: 'shared',
+		},
+		footer: {
+			import: './modules/footer/footer.js',
+			dependOn: 'shared',
+		},
+		shared: 'jquery',
+	},
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: '[name].bundle.js',
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+		},
+	},
+	devServer: {
+		static: path.join(__dirname, './public'),
+		open: true,
+		port: 8564,
+	},
+	performance: {
+		maxAssetSize: 1000000,
+	},  
+  module: {
+	rules: [
+		{
+			test: /\.css$/i,
+			use: ["css-loader", "style-loader"],
+		},
+		{
+			test: /\.(?:ico|gif|png|jpe?g|svg)$/i,
+			type: 'asset/resource',
+			use: [
+				"file-loader",
+				{
+					loader: "image-webpack-loader",
+					options: {
+							bypassingOnDebug: true,
+							disable: true,
+					},
+				},
+			],
+		},
+	],
+},
 };
