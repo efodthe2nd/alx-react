3bb4be90939efd07eed018e1fa5e58976db228b7
David Efod <davidefod@gmail.com>
2023-08-01 17:16:52 +0100

    Task_3

--- 0x00-react_introduction/task_2/dashboard/src/index.js
+++ 0x00-react_introduction/task_2/dashboard/src/index.js
@@ -4,15 +4,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Notifications from "./Notifications";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
 );
 
-const rootNotifications = document.getElementById("root-notifications");
-ReactDOM.render(
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
   <React.StrictMode>
     <Notifications />
   </React.StrictMode>
