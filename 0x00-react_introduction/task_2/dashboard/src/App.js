0fd7414b38955390e4c8b695918eb423f635b982
David Efod <davidefod@gmail.com>
2023-08-01 13:25:39 +0100

    task_2

--- 0x00-react_introduction/task_2/dashboard/src/App.js
+++ 0x00-react_introduction/task_2/dashboard/src/App.js
@@ -11,9 +11,16 @@ function App() {
         <h1>School dashboard</h1>
       </header>
 
-      <body className="App-body">
+      <div className="App-body">
         <p> Login to access the full dashboard </p>
-      </body>
+        <form>
+          <label htmlFor='email'>Email:</label>
+          <input type="email" name='email'></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name='password'></input>
+          <button>OK</button>
+        </form>
+      </div>
 
       <footer className="App-footer">
         {getFullYear()}
