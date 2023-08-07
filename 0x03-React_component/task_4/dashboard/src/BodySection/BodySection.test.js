b746e4f42a322d405fd9b601b1b927213464a849
David Efod <davidefod@gmail.com>
2023-08-07 17:16:53 +0100

    initial commit

--- 0x03-React_component/task_4/dashboard/src/BodySection/BodySection.test.js
+++ 0x03-React_component/task_4/dashboard/src/BodySection/BodySection.test.js
@@ -3,17 +3,18 @@ import BodySection from "./BodySection";
 import { shallow } from "enzyme";
 
 describe("BodySection tests", () => {
-    it("should render correctly", () => {
-        const wrapper = shallow(
-            <BodySection title="test title">
-                <p>test children node</p>
-            </BodySection>
-        );
+  it("should render correctly", () => {
+    const wrapper = shallow(
+      <BodySection title="test title">
+        <p>test children node</p>
+      </BodySection>
+    );
 
-        expect(wrapper.exists()).toBe(true);
-        expect(wrapper.exists("h2")).toBe(true);
-        expect(wrapper.find("h2").html()).toEqual("<h2>test title</h2>");
-        expect(wrapper.exists("p")).toBe(true);
-        expect(wrapper.find("p").text()).toEqual("test children node");
-    });
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.exists("h2")).toBe(true);
+    expect(wrapper.find("h2").html()).toEqual("<h2>test title</h2>");
+    expect(wrapper.exists("p")).toBe(true);
+    expect(wrapper.find("p").text()).toEqual("test children node");
+  });
 });
+
