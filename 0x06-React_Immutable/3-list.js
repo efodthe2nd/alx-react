39c7971fc9f82d15013cb9ecede9e7bd184d127a
David Efod <davidefod@gmail.com>
2023-08-15 09:49:55 +0000

    3

---
+++ 0x06-React_Immutable/3-list.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { List } = require('immutable');
+
+export function getListObject(array) {
+  return List(array);
+}
+
+export function addElementToList(list, element) {
+  return list.push(element);
+}
