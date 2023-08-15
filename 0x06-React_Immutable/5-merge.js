2aedcb0d30596f0f4ee78019ba1f00310ad6638d
David Efod <davidefod@gmail.com>
2023-08-15 10:13:46 +0000

    5

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { List, Map } = require('immutable');
+
+function concatElements(page1, page2) {
+  return List(page1).concat(List(page2));
+}
+
+function mergeElements(page1, page2) {
+  return Map(page1).merge(Map(page2));
+}
